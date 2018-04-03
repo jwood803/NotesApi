bd90216ff761e8c6d183ee8364fe3c3deb1888a5
J W <woodjon@gmail.com>
2018-04-03 14:13:26 -0400

    Initial add of project

---
+++ NotesApi/Controllers/NotesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NotesApi.Models;
+
+namespace NotesApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class NotesController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Note> GetNotes()
+        {
+            return new List<Note>
+            {
+                new Note { Id=1, Details="Details", Title="New book" }
+            };
+        }
+
+        [HttpPost]
+        public bool AddNote(Note note)
+        {
+            return true;
+        }
+
+        [HttpPut]
+        public bool EditNote(Note note)
+        {
+            return true;
+        }
+
+        [HttpDelete]
+        public bool DeleteNote(int noteId)
+        {
+            return true;
+        }
+    }
+}
---
+++ NotesApi/Models/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesApi.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public int Rating { get; set; }
+        public bool IsFavorite { get; set; }
+    }
+}
